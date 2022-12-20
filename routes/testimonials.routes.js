698becb90d067054e8bd9e819753432087cce42f
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-20 09:40:08 +0100

    add changes

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../src/db.js');
+const shortid = require('shortid');
 
 
-router.route('/testimonials').get();
+router.route('/testimonials').get(function(req, res) {
+    res.json(db.testimonials);
+});
 
-router.route('/testimonials/random').get();
+router.route('/testimonials/random').get(function(req, res) {
+    const randomIndex = Math.floor(Math.random() * db.testimonials.length);
+    res.json(db.testimonials[randomIndex]);
+});
 
-router.route('/testimonials/:id').get();
+router.route('/testimonials/:id').get(function(req, res) {
+    res.json(db.testimonials.find(testimonial => testimonial.id === req.params.id))
+});
 
-router.route('/testimonials').post();
+router.route('/testimonials').post(function(req, res) {
+    const newTestimonial = {
+        id: shortid(),
+        author: req.body.author,
+        text: req.body.text
+    };
+    db.testimonials.push(newTestimonial)
+    res.json(newTestimonial);
+});
 
-router.route('/testimonials/:id').put();
+router.route('/testimonials/:id').put(function(req, res) {
+    const testimonialIndex = db.testimonials.findIndex(tes => tes.id === req.params.id);
+    db.testimonials[testimonialIndex] = {
+        id: db.testimonials[testimonialIndex].id,
+        author: req.body.author,
+        text: req.body.text
+    }
 
-router.route('/testimonials/:id').delete();
+    res.json({
+        success: true
+    });
+});
+
+router.route('/testimonials/:id').delete(function(req, res) {
+    const testimonialIndex = db.testimonials.findIndex(tes => tes.id === req.params.id);
+    db.testimonials.splice(testimonialIndex, 0);
+
+    res.json({
+        success: true
+    });
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,45 +13,11 @@ app.use(cors());
 
 app.use('/api', testimonialsRoutes);
 
-app.get('/testimonials',(req, res)=>{
-  res.render('')
-});
-
-app.get('/testimonials/:id',(req, res)=>{
-  res.render('')
-});
-
-app.get('/testimonials/random',(req, res)=>{
-  res.render('')
-});
-
-app.post('testimonials', (req, res) => {
-
-  const { id, author, text } = req.body;
-
-  if(id && author && text) {
-    res.send('OK');
-  }
-  else {
-    res.send('error')
-  }
-
-});
-
-app.put('/testimonials',(req, res)=>{
-  res.render('OK')
-});
-
-app.delete('/testimonials/:id',(req, res)=>{
-  res.render('OK')
-});
-
-
 app.use(( req, res ) => {
-  res.status(400).send('404 not found...');
+res.status(400).send('404 not found...');
 });
 
 
 app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-  });
+console.log('Server is running on port: 8000');
+});
