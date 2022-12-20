7edd7ccafa1de0d9a481f6eb9d53b600a16ce435
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-20 09:19:36 +0100

    add changes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', testimonialsRoutes);
 
