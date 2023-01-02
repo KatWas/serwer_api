5c1e446227d93d48a490e0fd4ac56ec25d13fab5
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-01-02 10:42:37 +0100

    add changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-class App extends React.Component {
+import { Routes, Route } from 'react-router-dom';
 
-  render() {
-    return (
-      <MainLayout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/prices" exact component={Prices} />
-          <Route path="/order-a-ticket" exact component={Order} />
-          <Route component={NotFound} />
-        </Switch>
-      </MainLayout>
-    );
-  }
+import MainLayout from './components/layout/MainLayout/MainLayout';
 
-}
+// import routes
+import Home from './components/pages/Home/HomePage';
+import NotFound from './components/pages/NotFound/NotFoundPage';
+import Prices from './components/pages/Prices/PricesPage';
+import Order from './components/pages/Order/OrderPage.js';
+
+const App = () => (
+  <MainLayout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/prices" element={<Prices />} />
+      <Route path="/order-a-ticket" element={<Order />} />
+      <Route element={<NotFound />} />
+    </Routes>
+  </MainLayout>
+);
 
 export default App;
--- client/src/components/features/OrderTicketForm/OrderTicketForm.js
+++ client/src/components/features/OrderTicketForm/OrderTicketForm.js
@@ -1,112 +1,97 @@
-import React from 'react';
 import { Button, Form, FormGroup, Label, Input, Row, Col, Alert, Progress } from 'reactstrap';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addSeatRequest, getRequests } from '../../../redux/seatsRedux';
 
 import './OrderTicketForm.scss';
-import SeatChooser from './../SeatChooser/SeatChooserContainer';
+import SeatChooser from './../SeatChooser/SeatChooser';
 
-class OrderTicketForm extends React.Component {
+const OrderTicketForm = () => {
+  const dispatch = useDispatch();
+  const requests = useSelector(getRequests);
+  console.log(requests);
 
-  state = {
-    order: {
-      client: '',
-      email: '',
-      day: 1,
-      seat: '',
-    },
-    isError: false,
-  }
+  const [order, setOrder] = useState({
+    client: '',
+    email: '',
+    day: 1,
+    seat: ''
+  });
+  const [isError, setIsError] = useState(false);
 
-  updateSeat = (e, seatId) => {
-    const { order } = this.state;
-
+  const updateSeat = (e, seatId) => {
     e.preventDefault();
-    this.setState({ order: { ...order, seat: seatId }});
+    setOrder({ ...order, seat: seatId });
   }
 
-  updateTextField = ({ target }) => {
-    const { order } = this.state;
+  const updateTextField = ({ target }) => {
     const { value, name } = target;
-
-    this.setState({ order: { ...order, [name]: value }});
+    setOrder({ ...order, [name]: value });
   }
 
-  updateNumberField = ({ target }) => {
-    const { order } = this.state;
+  const updateNumberField = ({ target }) => {
     const { value, name } = target;
-
-    this.setState({ order: { ...order, [name]: parseInt(value) }});
+    setOrder({ ...order, [name]: parseInt(value) });
   }
 
-  submitForm = async (e) => {
-    const { order } = this.state;
-    const { addSeat } = this.props;
-
+  const submitForm = async (e) => {
     e.preventDefault();
 
     if(order.client && order.email && order.day && order.seat) {
-      await addSeat(order);
-      this.setState({ 
-        order: {
-          client: '',
-          email: '',
-          day: 1,
-          seat: '',
-        },
-        isError: false,
+      dispatch(addSeatRequest(order));
+      setOrder({
+        client: '',
+        email: '',
+        day: 1,
+        seat: '',
       });
+      setIsError(false);
     } else {
-      this.setState({ isError: true });
+      setIsError(true);
     }
   }
 
-  render() {
-
-    const { updateSeat, updateTextField, updateNumberField, submitForm } = this;
-    const { requests } = this.props;
-    const { order, isError } = this.state;
-
-    return (
-      <Form className="order-ticket-form" onSubmit={submitForm}>
-        <Row>
-          <Col xs="12" md="6">
-            { (isError) && <Alert color="warning">There are some errors in your form. Have you fill all the fields? Maybe you forgot to choose your seat?</Alert> }
-            { (requests['ADD_SEAT'] && requests['ADD_SEAT'].error && !isError) && <Alert color="danger">{requests['ADD_SEAT'].error}</Alert> }
-            { (requests['ADD_SEAT'] && requests['ADD_SEAT'].success && !isError) && <Alert color="success">You've booked your ticket! Check your email in order to make a payment.</Alert> }
-            { (requests['ADD_SEAT'] && requests['ADD_SEAT'].pending) && <Progress animated className="mb-5" color="primary" value={75} /> }
-            <FormGroup>
-              <Label for="clientEmail">Name</Label>
-              <Input type="text" value={order.client} name="client" onChange={updateTextField} id="clientName" placeholder="John Doe" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="clientEmail">Email</Label>
-              <Input type="email" value={order.email} name="email" onChange={updateTextField} id="clientEmail" placeholder="johndoe@example.com" />
-            </FormGroup>
-            <FormGroup>
-              <Label for="clientDay">Select which day of festivals are you interested in:</Label>
-              <Input type="select" value={order.day} name="day" onChange={updateNumberField} id="exampleSelect">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-              </Input>
-              <small id="dayHelp" className="form-text text-muted">Every day of the festival uses individual ticket. You can book only one ticket at the time.</small>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input required type="checkbox" /> I agree with <a href="#">Terms and conditions</a> and <a href="#">Privacy Policy</a>.
-              </Label>
-            </FormGroup>
-            <Button color="primary" className="mt-3">Submit</Button>
-          </Col>
-          <Col xs="12" md="6">
-            <SeatChooser 
-              chosenDay={order.day}
-              chosenSeat={order.seat} 
-              updateSeat={updateSeat} />
-          </Col>
-        </Row>
-      </Form>
-    )
-  };
+  return (
+    <Form className="order-ticket-form" onSubmit={submitForm}>
+      <Row>
+        <Col xs="12" md="6">
+          { (isError) && <Alert color="warning">There are some errors in you form. Have you fill all the fields? Maybe you forgot to choose your seat?</Alert> }
+          { (requests['ADD_SEAT'] && requests['ADD_SEAT'].error && !isError) && <Alert color="danger">{requests['ADD_SEAT'].error}</Alert> }
+          { (requests['ADD_SEAT'] && requests['ADD_SEAT'].success && !isError) && <Alert color="success">You've booked your ticket! Check you email in order to make a payment.</Alert> }
+          { (requests['ADD_SEAT'] && requests['ADD_SEAT'].pending) && <Progress animated className="mb-5" color="primary" value={75} /> }
+          <FormGroup>
+            <Label for="clientEmail">Name</Label>
+            <Input type="text" value={order.client} name="client" onChange={updateTextField} id="clientName" placeholder="John Doe" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="clientEmail">Email</Label>
+            <Input type="email" value={order.email} name="email" onChange={updateTextField} id="clientEmail" placeholder="johndoe@example.com" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="clientDay">Select which day of festivals are you interested in:</Label>
+            <Input type="select" value={order.day} name="day" onChange={updateNumberField} id="exampleSelect">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </Input>
+            <small id="dayHelp" className="form-text text-muted">Every day of the festival uses individual ticket. You can book only one ticket at the time.</small>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input required type="checkbox" /> I agree with <a href="/terms-and-conditions">Terms and conditions</a> and <a href="/privacy-policy">Privacy Policy</a>.
+            </Label>
+          </FormGroup>
+          <Button color="primary" className="mt-3">Submit</Button>
+        </Col>
+        <Col xs="12" md="6">
+          <SeatChooser 
+            chosenDay={order.day}
+            chosenSeat={order.seat} 
+            updateSeat={updateSeat} />
+        </Col>
+      </Row>
+    </Form>
+  )
 }
 
 export default OrderTicketForm;
