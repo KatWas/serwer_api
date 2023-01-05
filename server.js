d38f91a3d755cd5ff999402ba104e94242712a87
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-01-05 13:59:38 +0100

    add changes

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.listen(process.env.PORT || 8000, () => {
  console.log('Server is running on port: 8000');
   });
 
-const io = socket(server);
+  const io = socket(server);
 
-io.on('connection', (socket) => {
-  console.log('New client! Its id – ' + socket.id);
-})
+  io.on('connection', (socket) => {
+    console.log('New client! Its id -' + socket.id);
+  })
